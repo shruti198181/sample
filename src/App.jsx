70e085096f1f81e433d2481498b766a6c35f426d
shruti198181 <shahshruti503@gmail.com>
2025-06-13 12:59:21 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter } from 'react-router-dom';
 import Mynavbar from './component/navbar';
 import Footer from './component/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <BrowserRouter>
