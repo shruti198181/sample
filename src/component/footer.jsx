924198252b8d0e2545d3092fbac3f46dfd3fa864
shruti198181 <shahshruti503@gmail.com>
2025-06-13 12:19:09 +0530

    changes

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -11,8 +11,9 @@ import { FaGooglePlay } from "react-icons/fa";
 function Footer() {
   return (
     <footer className="  py-4 mt-5">
-        <div className="d-flex justify-content-center " style={{margin:'10px',width:'100vw'}}>
-          <div style={{ width: "300px", margin: "10px" }}className="col-xs-12 co,-md-3 col-md-6">
+      <Container>
+        <Row className="d-flex justify-content-center " style={{margin:'10px',width:'100vw'}}>
+          <Col md={3} sm={6} xs={12} style={{ width: "300px", margin: "10px" }}>
             <h5 className="text-danger">E-Commerce</h5>
             <div >
               <FaInstagramSquare size={22} style={{color:'grey'}}/>
@@ -21,23 +22,23 @@ function Footer() {
               <FaLinkedinIn size={22} style={{color:'grey', margin:'20px'}}/>
              </div>
              <p>© KiranaKart Technologies Private Limited</p>
-          </div>
-          <div style={{ width: "300px", margin: "10px" }} className="ms-5 col-xs-6 col-md-3 col-sm-6">
+          </Col>
+          <Col  md={3} sm={6} xs={6} style={{ width: "300px", margin: "10px" }}>
             <p>Home</p>
             <p>Delivery Areas</p>
             <p>Careeres</p>
             <p>Custmors Supports</p>
             <p>Press</p>
-          </div>
-          <div style={{ width: "300px", margin: "10px" }} className="col-xs-6 col-md-3 col-sm-6">
+          </Col>
+          <Col  md={3} sm={6} xs={6} style={{ width: "300px", margin: "10px" }}>
             <p>Privacy Policy</p>
             <p>Terms of Uses</p>
             <p>Responsible Disclosure Policy</p>
             <p>Mojon-a Zepto Blog</p>
             <p>Sell on Zepto</p>
             <p>Delivery with Zepto</p>
-          </div>
-          <div style={{ width: "300px", margin: "10px" }} className="col-md-3 col-sm-6 col-xs-12">
+          </Col>
+          <Col md={3} sm={6} xs={12} style={{ width: "300px", margin: "10px" }}>
             <p>Download App</p>
             <input 
             type="playstore"
@@ -48,9 +49,9 @@ function Footer() {
             placeholder="Get it on app store"
             aria-label="app"/>
             {/* <input type="" */}
-          </div>
-        </div>
-    
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 };
